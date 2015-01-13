95396e4e7a1b48f81851bf12318fe59881373227
xxxnemesis4xxx <magnitude9@live.ca>
2015-01-13 04:05:57 -0500

    Create a Pair with move semantic

--- CPlusPlusReference/pair.cpp
+++ CPlusPlusReference/pair.cpp
@@ -3,6 +3,8 @@
 #include <tuple>
 #include <iostream>
 #include <QString>
+#include <string>
+#include <functional>
 
 using namespace std;
 
@@ -36,8 +38,24 @@ QString Pair::CreatePairWithPairMakeExample()
     return display;
 }
 
-QString Pair::MovingPairValueWithSemanticExample();
-QString Pair::EnforceRefMoveSemanticWithPairExample();
+QString Pair::MovingPairValueWithSemanticExample()
+{
+    QString display = "Moving variable s and t in a pair\n";
+    std::string s = "Hello";
+    std::string t = "World";
+
+    auto p = std::make_pair(std::move(s),std::move(t));
+
+    display += "Value inside pair = (" + p.first + "," + p.second + ")\n";
+    display += "Value inside s and t now.\ns = " + QString::fromStdString(s) + "\nt = " + QString::fromStdString(t);
+
+    return display;
+}
+
+QString Pair::EnforceRefMoveSemanticWithPairExample()
+{
+}
+
 QString Pair::EnforceRefMoveSemanticWithTupleExample();
 QString Pair::MakePairWithDefinedTypeExample();
 QString Pair::ExtractSpecificValueFromPairExample();
@@ -54,15 +72,19 @@ QString Pair::DisplayPairValueExampleCode()
 QString Pair::CreatePairWithPairMakeCode()
 {
     QString display = QString(
-                "pair newPair = std::make_pair('C',3.1416)\n" +
-                "display += QString(\"Pair values = ('\" + to_string(newPair.first) + \"',\" + "
-                "to_string(newPair.second) + \")\");"
+                "std::pair newPair = std::make_pair('C',3.1416);\n";
                 );
 
     return display;
 }
 
-QString Pair::MovingPairValueWithSemanticCode();
+QString Pair::MovingPairValueWithSemanticCode()
+{
+    QString display = "auto p = std::make_pair(std::move(s),std::move(t));";
+
+    return display;
+}
+
 QString Pair::EnforceRefMoveSemanticWithPairCode();
 QString Pair::EnforceRefMoveSemanticWithTupleCode();
 QString Pair::MakePairWithDefinedTypeCode();
