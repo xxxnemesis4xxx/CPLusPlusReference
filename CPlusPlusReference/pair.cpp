2c02f4d4b49f89da21bab7ddd6456a585780d4af
xxxnemesis4xxx <magnitude9@live.ca>
2015-01-13 03:44:25 -0500

    Display Pair value

--- CPlusPlusReference/pair.cpp
+++ CPlusPlusReference/pair.cpp
@@ -1,12 +1,51 @@
 #include "pair.h"
+#include <utility>
+#include <tuple>
+#include <iostream>
+#include <QString>
+
+using namespace std;
 
 Pair::Pair()
 {
-
 }
 
 Pair::~Pair()
 {
-
 }
 
+QString Pair::DisplayPairValueExample()
+{
+    QString display;
+
+    //Display the elements inside the PairStruct
+    pair<int,int> PairStruct(5,6);
+    display = QString(to_string(PairStruct.first) + " " + to_string(PairStruct.second));
+
+    return display;
+}
+
+QString Pair::ReturnSizePairExample();
+QString Pair::CreatePairWithPairMakeExample();
+QString Pair::MovingPairValueWithSemanticExample();
+QString Pair::EnforceRefMoveSemanticWithPairExample();
+QString Pair::EnforceRefMoveSemanticWithTupleExample();
+QString Pair::MakePairWithDefinedTypeExample();
+QString Pair::ExtractSpecificValueFromPairExample();
+
+QString Pair::DisplayPairValueExampleCode()
+{
+    QString display =
+            QString("std::pair<int,int> PairStruct(5,6);\n")
+            + QString("display = QString(to_string(PairStruct.first) + QString(PairStruct.second));");
+
+    return display;
+}
+
+QString Pair::ReturnSizePairCode();
+QString Pair::CreatePairWithPairMakeCode();
+QString Pair::MovingPairValueWithSemanticCode();
+QString Pair::EnforceRefMoveSemanticWithPairCode();
+QString Pair::EnforceRefMoveSemanticWithTupleCode();
+QString Pair::MakePairWithDefinedTypeCode();
+QString Pair::ExtractSpecificValueFromPairCode();
